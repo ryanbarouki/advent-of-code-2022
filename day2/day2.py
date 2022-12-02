fa7ffda9040b68ad87d907d62cb0c9fe668eda78
Ryan Barouki <ryanbarouki@gmail.com>
2022-12-02 14:35:03 +0000

    make it less awful

--- day2/day2.py
+++ day2/day2.py
@@ -1,35 +1,41 @@
-def score_for_first_player(p1, p2):
-    diff = (p1 - p2) % 3
-    if diff == 1:
-        #win
-        return p1 + 6
-    elif diff == 0:
-        #draw
-        return p1 + 3
-    else:
-        #lose
-        return p1
+import enum
 
-def player_move_given_outcome(p1, outcome):
-    if outcome == 2:
-        # draw
+class Move(enum.Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+class Outcome(enum.Enum):
+    DRAW = 0
+    WIN = 1
+    LOSE = 2
+
+def score_for_first_player(p1: Move, p2: Move):
+    outcome = Outcome((p1.value - p2.value) % 3)
+    if outcome == Outcome.WIN:
+        return p1.value + 6
+    elif outcome == Outcome.DRAW:
+        return p1.value + 3
+    else:
+        return p1.value
+
+def player_move_given_outcome(p1: Move, outcome: Outcome):
+    if outcome == Outcome.DRAW:
         return p1
-    elif outcome == 1:
-        # lose
-        return (p1 - 2) % 3 + 1
+    elif outcome == Outcome.LOSE:
+        return Move((p1.value - 2) % 3 + 1)
     else:
         # win
-        return p1 % 3 + 1
+        return Move(p1.value % 3 + 1)
 
-
-p1_decode_dict = {'A': 1, 'B': 2, 'C':3}
-outcome_decode_dict = {'X': 1, 'Y': 2, 'Z':3}
+col1_dict = {'A': Move.ROCK, 'B': Move.PAPER, 'C': Move.SCISSORS}
+col2_dict = {'X': Outcome.LOSE, 'Y': Outcome.DRAW, 'Z': Outcome.WIN}
 
 with open('input.txt') as f:
     total = 0
     for line in f.readlines():
         line = line.strip()
-        p1, outcome = line.split(" ")
-        p2_move = player_move_given_outcome(p1_decode_dict[p1], outcome_decode_dict[outcome])
-        total += score_for_first_player(p2_move, p1_decode_dict[p1])
+        p1_move, outcome = line.split(" ")
+        p2_move = player_move_given_outcome(col1_dict[p1_move], col2_dict[outcome])
+        total += score_for_first_player(p2_move, col1_dict[p1_move])
     print(total)
