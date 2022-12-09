5ee779b0488569753d8ee4b96523dc28471eed85
Ryan Barouki <ryanbarouki@gmail.com>
2022-12-09 12:31:20 +0000

    add an animation for day 9 rope

--- day9/day9.py
+++ day9/day9.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
 def too_close(head_pos, tail_pos):
     head_x, head_y = head_pos
     tail_x, tail_y = tail_pos
@@ -52,17 +55,36 @@ def find_tail_positions(instructions, rope_length):
         tail_positions.add(rope[rope_length-1])
     return tail_positions
 
+def animate(rope, instruction, mat):
+    rope = update_rope(rope, instruction)
+    x,y = zip(*rope)
+    mat.set_data(x,y)
+    return mat,
+
 with open('input.txt') as f:
     instructions = []
     for line in f.readlines():
         line = line.strip()
         direction, number = line.split(" ")
-        instructions += [direction for i in range(int(number))]
+        instructions += [direction for _ in range(int(number))]
     
     tail_positions_p1 = find_tail_positions(instructions, 2)
     tail_positions_p2 = find_tail_positions(instructions, 10)
             
     print(f"Part 1: Number of positions visited: {len(tail_positions_p1)}")
     print(f"Part 2: Number of positions visited: {len(tail_positions_p2)}")
+
+    fig, ax = plt.subplots()
+
+    rope = [(0,0) for i in range(10)]
+    x,y = zip(*rope)
+    mat, = ax.plot(x,y,'.', markersize=10)
+
+    ani = animation.FuncAnimation(fig, lambda i: animate(rope, instructions[i], mat), interval=50)
+    ax.axis([-30,30,-30, 30])
+    plt.show()
+
+
+
 
     
