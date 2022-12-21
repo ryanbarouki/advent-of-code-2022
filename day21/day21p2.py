74371694f26eb1b3bd01082151aa2383d847af26
Ryan Barouki <ryanbarouki@gmail.com>
2022-12-21 10:50:30 +0000

    day 21 part 2 but I'm going to hell for it

--- day21/day21p2.py
+++ day21/day21p2.py
@@ -1,9 +1,7 @@
-
 import re
 
 with open('input.txt') as f:
     monkeys = {}
-    ops = {'+': lambda a,b: a+b, "-": lambda a,b: a-b, "*": lambda a,b: a*b, "/": lambda a,b: a/b}
     for line in f.readlines():
         line = line.strip()
         monkey, operation = line.split(":")
@@ -13,18 +11,21 @@ with open('input.txt') as f:
             m_num = int(m_num.strip())
             if monkey == 'humn':
                 # that's you!
-                monkeys[monkey] = lambda humn: humn
+                monkeys[monkey] = lambda: "x"
                 continue
-            monkeys[monkey] = lambda humn, m_num=m_num: m_num
+            monkeys[monkey] = lambda m_num=m_num: m_num
         else:
             first, second = mops
             first, second = first.strip(), second.strip()
             opc = operation.split(" ")[2]
-            op = ops[opc]
             if monkey == 'root':
-                monkeys['root'] = lambda humn, first=first, second=second, op=op: monkeys[first](humn) == monkeys[second](humn)
+                monkeys['root'] = lambda first=first, second=second: f"{monkeys[first]()} == {monkeys[second]()}"
                 continue
-            monkeys[monkey] = lambda humn, first=first, second=second, op=op: op(monkeys[first](humn), monkeys[second](humn))
+            monkeys[monkey] = lambda first=first, second=second, opc=opc: f"({monkeys[first]()} {opc} {monkeys[second]()})"
 
-
-    print(monkeys['root'](10302))
+    file = open('output.txt', 'w')
+    file.write(monkeys['root']())
+    # I stuck this output in Mathematica Solve[...,{x}]. I know I am going to AoC hell for this.
+    # I didn't know a priori whether it would be a linear function of x - which it is
+    # So in theory I would have to write a symbolic solver?!?
+    # May come back to this but I'm happy to take the star and move on with my life
